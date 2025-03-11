ee1b0858e2bb63fa5c111b13b9623cf13652387a
wilmar lopez <wilopez8@yahoo.com>
2025-03-11 17:05:23 -0400

    Primer commit de la aplicacion para obtener la ubicacion de las personas

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
+import datetime
+import json
+import time
+
+st.header("Ubicacion")
+st.write("Por favor de click en el boton, tome pantallazo y compartalo para certificar la ubicacion.") 
+
+# Display the current date and time
+current_date  = datetime.datetime.now().strftime("%m-%d-%Y") 
+current_time  = datetime.datetime.now().strftime("%H:%M") 
+
+loc = get_geolocation() # Get the geolocation of the user
+
+# Extract latitude and longitude
+latitude = loc['coords']['latitude']
+longitude = loc['coords']['longitude']
+
+# Button to fetch location details
+if st.button("Ubicación y Hora"):
+    
+    # Display the current date and time
+    st.write(f"Fecha: {current_date}")
+    st.write(f"Hora: {current_time}")
+
+    # Create a Google Maps link
+    st.write(f"Latitude: {latitude}")
+    st.write(f"Longitude: {longitude}")
+    st.write(f"[Google Maps](https://www.google.com/maps/search/?api=1&query={latitude},{longitude})")
+    # Display the latitude and longitude
