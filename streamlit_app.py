0223f2d1b7d7283f67fb902ff1226db823067e58
wilmar lopez <wilopez8@yahoo.com>
2025-03-11 18:31:13 -0400

    tercer commit para incluir el map en la pantalla

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share
 import datetime
 import json
 import time
+import pandas as pd
+
 
 st.header("Ubicacion")
 st.write("Por favor de click en el boton, tome pantallazo y compartalo para certificar la ubicacion.") 
@@ -11,9 +13,9 @@ st.write("Por favor de click en el boton, tome pantallazo y compartalo para cert
 current_date  = datetime.datetime.now().strftime("%m-%d-%Y") 
 current_time  = datetime.datetime.now().strftime("%H:%M") 
 
-loc = get_geolocation() # Get the geolocation of the user
-
+# Get the location data
 # Extract latitude and longitude
+loc = get_geolocation() 
 latitude = loc['coords']['latitude']
 longitude = loc['coords']['longitude']
 
@@ -29,3 +31,10 @@ if st.button("Ubicación y Hora"):
     st.write(f"Longitude: {longitude}")
     st.write(f"[Google Maps](https://www.google.com/maps/search/?api=1&query={latitude},{longitude})")
     # Display the latitude and longitude
+
+data = pd.DataFrame({
+    'lat': [latitude],  # Latitude values
+    'lon': [longitude]  # Use negative values for west
+})
+
+st.map(data)
